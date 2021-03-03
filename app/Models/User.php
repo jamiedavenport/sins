669bf801ed73c08b85530608f31ddf687907d693
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-03-03 22:29:50 +0000

    Add Sin model

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sins() {
+        return $this->hasMany(Sin::class);
+    }
 }
